1a2cb621fc50594e4e207215b79e476f2076072d
cpatelis <patelischristos@gmail.com>
2024-04-02 13:14:20 +0300

    cl-camp4

--- myfamily.sh
+++ myfamily.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fetch the superhero data from the JSON API and extract the family of the superhero with the given ID
-family=$(curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r --arg HERO_ID "$HERO_ID" '.[] | select(.id == ($HERO_ID|tonumber)) | .relatives')
+family=$(curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r --arg HERO_ID "$HERO_ID" '.[] | select(.id == ($HERO_ID|tonumber)) | .connections.relatives')
 
 # Remove quotes from the family string
 family=$(echo "$family" | tr -d '"')
