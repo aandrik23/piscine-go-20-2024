351b9b3d1db41b205c3cfb96c2ae4f7e90d07fe7
cpatelis <patelischristos@gmail.com>
2024-04-02 11:05:32 +0300

    Who are you

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Fetch data from the JSON API and extract the name of the superhero with ID 70
-curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r '.[] | select(.id == 70) | .name'
-
+curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r '.[] | select(.id == 70) | "\"\(.name)\""'
