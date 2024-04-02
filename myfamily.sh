e6dc16c403af650f564e4179634f552362097048
cpatelis <patelischristos@gmail.com>
2024-04-02 13:48:24 +0300

    cl-camp4

--- myfamily.sh
+++ myfamily.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-# Check if HERO_ID environment variable is set
-if [ -z "$HERO_ID" ]; then
-  echo "HERO_ID environment variable is not set."
-  exit 1
-fi
-
-# Fetch data from the URL and parse JSON using jq
-family=$(curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r ".[] | select(.id == \"$HERO_ID\") | .connections.relatives")
-
-# Remove quotes from the relatives field
-family=$(echo "$family" | sed 's/"//g')
-
-# Display the family information
-echo "Family of Hero ID $HERO_ID:"
-echo "$family"
+curl -s https://platform.zone01.gr/assets/superhero/all.json | jq --arg HERO_ID "$HERO_ID" '.[] | select(.id== ($HERO_ID | tonumber)) | .connections.relatives' | tr -d "\""
