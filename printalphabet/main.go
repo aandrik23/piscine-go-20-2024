4449f6e22218ed05d4812de5ebfe70c6a2a46917
cpatelis <patelischristos@gmail.com>
2024-04-03 13:50:05 +0300

    my first go program

---
+++ printalphabet/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	// Iterate over each letter of the alphabet
+	for c := 'a'; c <= 'z'; c++ {
+		// Print the current letter
+		z01.PrintRune(c)
+	}
+	// Print a newline character after printing all the letters
+	z01.PrintRune('\n')
+}
