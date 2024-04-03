fa002777aabdd2b6816210e39b462cbac826b4b8
cpatelis <patelischristos@gmail.com>
2024-04-03 18:38:08 +0300

    print comb 2

--- printcomb2.go
+++ printcomb2.go
@@ -4,23 +4,20 @@ import "github.com/01-edu/z01"
 
 // PrintComb2 prints all possible combinations of two different two-digit numbers.
 func PrintComb2() {
-	var i, j int
-	for i = 0; i <= 98; i++ {
-		for j = i + 1; j <= 99; j++ {
-			if i < 10 {
-				z01.PrintRune('0')
-			}
-			z01.PrintRune(rune('0' + i))
-			z01.PrintRune(' ')
-			if j < 10 {
-				z01.PrintRune('0')
-			}
-			z01.PrintRune(rune('0' + j))
+	for i := 0; i <= 98; i++ {
+		for j := i + 1; j <= 99; j++ {
+			z01.PrintRune(rune('0' + i/10)) // Print tens digit of i
+			z01.PrintRune(rune('0' + i%10)) // Print units digit of i
+			z01.PrintRune(' ')              // Print a space between numbers
+			z01.PrintRune(rune('0' + j/10)) // Print tens digit of j
+			z01.PrintRune(rune('0' + j%10)) // Print units digit of j
+
+			// If not the last combination, print a comma and a space
 			if i != 98 || j != 99 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune('\n') // Print a newline at the end
 }
