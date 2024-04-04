a1772518f634f1239ea6a380a811818b511d3702
cpatelis <patelischristos@gmail.com>
2024-04-04 13:07:55 +0300

    print number

--- printnbr.go
+++ printnbr.go
@@ -1,10 +1,18 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 func PrintNbr(n int) {
-	// Handle negative numbers
-	if n < 0 {
+	if n == -9223372036854775808 {
+		// If n is the minimum possible integer value,
+		// print it as a string
+		str := "-9223372036854775808"
+		for _, ch := range str {
+			z01.PrintRune(ch)
+		}
+	} else if n < 0 {
 		z01.PrintRune('-')
 		printPositiveNumber(-n)
 	} else {
@@ -13,23 +21,19 @@ func PrintNbr(n int) {
 }
 
 func printPositiveNumber(n int) {
-	// Handle the case when n is 0
 	if n == 0 {
 		z01.PrintRune('0')
 		return
 	}
 
-	// Create a buffer to store digits as characters
 	buffer := make([]rune, 0)
 
-	// Extract digits of n and store them in the buffer
 	for n > 0 {
 		digit := n % 10
 		buffer = append(buffer, rune('0'+digit))
 		n /= 10
 	}
 
-	// Print digits in reverse order (since we extracted them in reverse order)
 	for i := len(buffer) - 1; i >= 0; i-- {
 		z01.PrintRune(buffer[i])
 	}
