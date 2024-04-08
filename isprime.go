6c82eddcde09d1f2e235c0d548951ff6c958be05
cpatelis <patelischristos@gmail.com>
2024-04-08 17:17:20 +0300

    isprime

--- isprime.go
+++ isprime.go
@@ -3,7 +3,9 @@ package piscine
 func IsPrime(nb int) bool {
 	i := 2
 
-	if nb == 2 {
+	if nb <= 0 {
+		return false
+	} else if nb == 2 {
 		return true
 	} else {
 		for i = 3; i <= nb; i = i + 2 {
