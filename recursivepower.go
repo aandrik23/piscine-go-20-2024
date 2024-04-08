bc612f629ea307a169656f223a07ffd8c415ebac
cpatelis <patelischristos@gmail.com>
2024-04-08 14:19:47 +0300

    recursivepower.go

--- recursivepower.go
+++ recursivepower.go
@@ -1,1 +1,15 @@
 package piscine
+
+func RecursivePower(nb int, power int) int {
+	var repo int = 1
+	a := 1
+	if power < 0 {
+		repo = 0
+	} else if power == 0 {
+		repo = 1
+	} else if a <= power {
+		a++
+		repo = repo * nb
+	}
+	return repo
+}
