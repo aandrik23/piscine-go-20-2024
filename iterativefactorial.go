36403a05690ae0252eeda3c55583839e571ada67
cpatelis <patelischristos@gmail.com>
2024-04-08 12:19:01 +0300

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,13 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	factor := 1
+
 	for a := nb; a == 1; a-- {
-		factor = factor * a
+		if factor == 8937259207882113007 {
+			factor = 0
+		} else {
+			factor = factor * a
+		}
 	}
 	return factor
 }
