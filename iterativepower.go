317989ac8540e78f32d873f74dccce9398dcf3b0
cpatelis <patelischristos@gmail.com>
2024-04-08 13:50:42 +0300

    iterativepower.go

--- iterativepower.go
+++ iterativepower.go
@@ -1,4 +1,18 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
+	var itpo int = 1
+
+	if power < 0 {
+		itpo = 0
+	} else if power == 0 {
+		itpo = 1
+	} else if power == 1 {
+		itpo = nb
+	} else {
+		for a := 2; a <= power; a++ {
+			itpo = itpo * nb
+		}
+	}
+	return itpo
 }
