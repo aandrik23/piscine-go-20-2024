02f88796ff08b24b5e8f79b944b846632ede26f8
cpatelis <patelischristos@gmail.com>
2024-04-08 12:22:27 +0300

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,11 +4,12 @@ func IterativeFactorial(nb int) int {
 	factor := 1
 
 	for a := nb; a == 1; a-- {
-		if factor == 8937259207882113007 {
-			factor = 0
-		} else {
-			factor = factor * a
-		}
+		factor = factor * a
+	}
+	if factor == 8937259207882113007 {
+		factor = 0
+	} else {
+		factor = factor * 1
 	}
 	return factor
 }
