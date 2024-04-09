c8e5def1c123d165ebfdd5823d48411f753bc705
cpatelis <patelischristos@gmail.com>
2024-04-09 15:44:27 +0300

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, v := range s {
-		if !(v >= 'A' && v <= 'Z' && v >= 'a' && v <= 'z' && v >= '0' && v <= '9' || v == ' ') {
+		if !((v >= 'A' && v <= 'Z') || (v >= 'a' && v <= 'z') || (v >= '0' && v <= '9') || v == ' ') {
 			return false
 		}
 	}
