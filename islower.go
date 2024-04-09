57b23fd6b4bfa33d6432854014c001cfa4a729bf
cpatelis <patelischristos@gmail.com>
2024-04-09 15:15:17 +0300

    islower

--- islower.go
+++ islower.go
@@ -1,14 +1,10 @@
 package piscine
 
 func IsLower(s string) bool {
-	var b bool
-
 	for _, v := range s {
 		if !(v >= 'a' && v <= 'z') {
-			b = false
-		} else {
-			b = true
+			return false
 		}
 	}
-	return b
+	return true
 }
