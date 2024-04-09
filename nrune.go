a85b682091c316bb7456313b9318a40968ff611d
cpatelis <patelischristos@gmail.com>
2024-04-09 11:58:34 +0300

    nrune

--- nrune.go
+++ nrune.go
@@ -2,9 +2,8 @@ package piscine
 
 func NRune(s string, n int) rune {
 	aa := []rune(s)
-	ab := len(s) - 1
 	ac := '0'
-	if n <= 0 || n > ab {
+	if n <= 0 || n > len(s) {
 		return ac
 	} else {
 		return aa[n-1]
