881d96e96b73d3179b9ee04c0f22fa30169b380b
cpatelis <patelischristos@gmail.com>
2024-04-11 07:51:54 +0300

    sortparams

---
+++ sortparams/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+
+	name := os.Args
+
+	for i := 0; i < len(name)-1; {
+		if name[i] > name[i+1] {
+			name[i], name[i+1] = name[i+1], name[i]
+			i = 0
+		} else {
+			i++
+		}
+	}
+	for _, word := range name {
+		for _, letter := range word {
+			z01.PrintRune(letter)
+		}
+		z01.PrintRune('\n')
+	}
+
+}
