3d450f191ec12ae4209e0429094b3006291992b2
cpatelis <patelischristos@gmail.com>
2024-04-12 13:30:55 +0300

    makerange

--- makerange.go
+++ makerange.go
@@ -4,13 +4,10 @@ func MakeRange(min, max int) []int {
 	if min >= max {
 		return []int{}
 	}
-
 	size := max - min
 	answer := make([]int, size)
-
 	for i := 0; i < size; i++ {
 		answer[i] = min + i
 	}
-
 	return answer
 }
