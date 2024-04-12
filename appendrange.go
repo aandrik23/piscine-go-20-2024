d93cff9d1cf6fe6bdcd21ef2dc46f6ef7e9248ff
cpatelis <patelischristos@gmail.com>
2024-04-12 12:40:58 +0300

    appendrange

--- appendrange.go
+++ appendrange.go
@@ -1,13 +1,12 @@
 package piscine
 
-func AppendRanintge(min, max int) []int {
+func AppendRange(min, max int) []int {
 
 	i := 0
 	var answer []int
 	if min > max {
 		answer = append(answer, 0)
 	} else {
-
 		for i = min; i < max; i++ {
 			answer = append(answer, i+1)
 		}
