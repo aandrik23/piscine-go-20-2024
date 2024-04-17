de0b53b3ac3398d667c31f9f05294bfb4c05a332
cpatelis <patelischristos@gmail.com>
2024-04-17 10:44:12 +0300

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("File name missing")
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+	} else {
+		name := os.Args[1]
+		file, err := os.Open(name)
+		if err != nil {
+			fmt.Printf("Error occured.Error is : %v\n", err.Error())
+		} else {
+			fileInfo, _ := file.Stat()
+			fileSize := fileInfo.Size()
+			text := make([]byte, fileSize)
+			file.Read(text)
+			fmt.Println(string(text))
+			file.Close()
+		}
+	}
+}
