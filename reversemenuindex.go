93145ff2a8808c87da4d8507c36aa3a3814e7253
cpatelis <patelischristos@gmail.com>
2024-04-19 02:50:28 +0300

    reverseorderindex

--- reversemenuindex.go
+++ reversemenuindex.go
@@ -2,7 +2,7 @@ package piscine
 
 func ReverseMenuIndex(menu []string) []string {
 	order := make([]string, len(menu))
-	for i := len(menu) - 1; i <= 0; i-- {
+	for i := len(menu) - 1; i >= 0; i-- {
 		order[i] = menu[len(menu)-1-i]
 	}
 	return order
