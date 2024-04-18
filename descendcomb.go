8aa8818c1bcf350dda7558438cf3f3d9b5fef8b1
cpatelis <patelischristos@gmail.com>
2024-04-18 12:14:55 +0300

    descendcomb

--- descendcomb.go
+++ descendcomb.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func DescendComb() {
 	for i := '9'; i >= '0'; i-- {
 		for j := '9'; j >= '0'; j-- {
-			for k := '9'; k >= '0'; k-- {
+			for k := i; k >= '0'; k-- {
 				var startL rune
 				if k == i {
 					startL = j - 1
