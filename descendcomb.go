60b0011bdf88c38e3e0133c64abafa28e2cce471
cpatelis <patelischristos@gmail.com>
2024-04-18 12:09:22 +0300

    descendcomb

---
+++ descendcomb.go
@@ -0,0 +1,32 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func DescendComb() {
+
+	for i := '9'; i >= '0'; i-- {
+		for j := '9'; j >= '0'; j-- {
+			for k := '9'; k >= '0'; k-- {
+				var startL rune
+				if k == i {
+					startL = j - 1
+				} else {
+					startL = '9'
+				}
+				for l := startL; l >= '0'; l-- {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+					if !(i == '0' && j == '1' && k == '0' && l == '0') {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
