341d08536832825813e23b0681e076e92a4ac9df
cpatelis <patelischristos@gmail.com>
2024-04-18 13:05:57 +0300

    unmatch

--- unmatch.go
+++ unmatch.go
@@ -1,29 +1,14 @@
 package piscine
 
-func IsNotPaired(n int, a []int) bool {
-	var counter int
-	for i := 0; i < len(a); i++ {
-		if n == a[i] {
-			counter++
-		}
-	}
-	if counter == 0 {
-		return true
-	}
-	if counter%2 == 0 {
-		return true
-	}
-	return false
-}
-
 func Unmatch(a []int) int {
-	var check []int
-	var result int
-	for i := 0; i < len(a)-1; i++ {
-		if IsNotPaired(a[i], a) {
-			check = append(check, a[i])
+	frequency := make(map[int]int)
+	for _, value := range a {
+		frequency[value]++
+	}
+	for _, number := range a {
+		if count, exists := frequency[number]; exists && count%2 != 0 {
+			return number
 		}
 	}
-	result = check[0]
-	return result
+	return -1
 }
