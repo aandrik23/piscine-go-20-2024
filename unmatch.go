8ad2bf89fcc3c47386411222ee3f496899508cb2
cpatelis <patelischristos@gmail.com>
2024-04-18 12:53:17 +0300

    unmatch

---
+++ unmatch.go
@@ -0,0 +1,28 @@
+package piscine
+
+func IsNotPaired(n int, a []int) bool {
+	var counter int
+	for i := 0; i < len(a); i++ {
+		if n == a[i] {
+			counter++
+		}
+	}
+	if counter == 0 {
+		return true
+	}
+	if counter%2 == 0 {
+		return true
+	}
+	return false
+}
+func Unmatch(a []int) int {
+	var check []int
+	var result int
+	for i := 0; i < len(a)-1; i++ {
+		if IsNotPaired(a[i], a) {
+			check = append(check, a[i])
+		}
+	}
+	result = check[0]
+	return result
+}
