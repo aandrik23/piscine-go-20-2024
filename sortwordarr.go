dcc15e05838d3d8d842d1e3458a31dd59a019497
cpatelis <patelischristos@gmail.com>
2024-04-19 03:26:55 +0300

    sortwordarr

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,12 +1,14 @@
 package piscine
 
 func SortWordArr(a []string) {
-	for i := 1; i <= len(a); {
-		if a[i-1] > a[i] {
-			a[i-1], a[i] = a[i], a[i-1]
-			i = 1
-		} else {
-			i++
+	if len(a) > 1 {
+		for i := 1; i <= len(a); {
+			if a[i-1] > a[i] {
+				a[i-1], a[i] = a[i], a[i-1]
+				i = 1
+			} else {
+				i++
+			}
 		}
 	}
 }
