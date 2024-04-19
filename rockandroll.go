9db5a7ba5bb29f324eab92ec44746f2b2224e2f8
cpatelis <patelischristos@gmail.com>
2024-04-19 08:45:54 +0300

    rockandroll

--- rockandroll.go
+++ rockandroll.go
@@ -3,14 +3,14 @@ package piscine
 func RockAndRoll(n int) string {
 	switch {
 	case n%2 == 0:
-		return "rock \n"
+		return "rock\n"
 	case n%3 == 0:
-		return "roll \n"
+		return "roll\n"
 	case n%2 == 0 && n%3 == 0:
-		return "rock and roll \n"
+		return "rock and roll\n"
 	case n < 0:
-		return "error: number is negative \n"
+		return "error: number is negative\n"
 	default:
-		return "error: non divisible \n"
+		return "error: non divisible\n"
 	}
 }
