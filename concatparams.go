ef184f39e33db836876d519ac2fae03d58b71424
cpatelis <patelischristos@gmail.com>
2024-04-24 14:12:55 +0300

    concatparams

--- concatparams.go
+++ concatparams.go
@@ -3,7 +3,11 @@ package piscine
 func ConcatParams(args []string) string {
 	var answer string
 	for i := 0; i < len(args); i++ {
-		answer += args[i] + "\n"
+		if i < len(args)-1 {
+			answer += args[i] + "\n"
+		} else {
+			answer += args[i]
+		}
 	}
 	return answer
 }
