5ca6a74e17ca2d2a02fc72fc633cb1cec87e2e4d
cpatelis <patelischristos@gmail.com>
2024-04-24 16:53:34 +0300

    splitwhitespaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -5,13 +5,10 @@ func SplitWhiteSpaces(s string) []string {
 	word := []byte(s)
 	var wordBuild string
 	for i := 0; i < len(s); i++ {
-		if word[i] != ' ' && word[i] != '\n' && word[i] != '\t' {
-			if i < len(s)-1 {
-				wordBuild = wordBuild + (string(word[i]))
-			} else {
-				answer = append(answer, wordBuild)
-			}
-		} else {
+		wordBuild = wordBuild + (string(word[i]))
+		if i == len(s)-1 {
+			answer = append(answer, wordBuild)
+		} else if word[i] == ' ' || word[i] == '\n' || word[i] == '\t' {
 			answer = append(answer, wordBuild)
 			wordBuild = ""
 		}
