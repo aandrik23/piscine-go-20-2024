37492f915916b09905b3dcf40eaf3a738d750e83
cpatelis <patelischristos@gmail.com>
2024-04-25 11:37:19 +0300

    splitwhitespaces

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,17 +1,20 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	var answer []string
-	word := []byte(s)
-	var wordBuild string
-	for i := 0; i < len(s); i++ {
-		wordBuild = wordBuild + (string(word[i]))
-		if i == len(s)-1 {
-			answer = append(answer, wordBuild)
-		} else if word[i] == ' ' || word[i] == '\n' || word[i] == '\t' {
-			answer = append(answer, wordBuild[:(len(wordBuild)-1)])
-			wordBuild = ""
+	var result []string
+	var word string
+	for _, ch := range s {
+		if ch == ' ' || ch == '\n' || ch == '\t' {
+			if word != "" {
+				result = append(result, word)
+				word = ""
+			}
+		} else {
+			word += string(ch)
 		}
 	}
-	return answer
+	if word != "" {
+		result = append(result, word)
+	}
+	return result
 }
